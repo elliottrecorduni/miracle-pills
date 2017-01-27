cf85f6c02e057f3c8b6b511a8ce75a591736031f
Elliott <s4922057@bournemouth.ac.uk>
2017-01-27 22:11:57 +0000

    Added Image Assets

---
+++ MiraclePill/MiraclePill/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MiraclePill
+//
+//  Created by Elliott Record on 27/01/2017.
+//  Copyright © 2017 Elliott Record. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.purple
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
